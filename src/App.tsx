cedf27f1a562219918cf8e257cc951136cb99f7e
ctshi <ctshi@sse.com.cn>
2022-02-23 21:12:51 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Pretty from "./components/Pretty";
 
 function App() {
+    const [prettyData,setPrettyData] = useState(
+        JSON.stringify(["'/foo'","'/bar'",{"baz":"baz","quz":{"quz":"quz"}},["'/baz'",["/qux"]]])
+    );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Pretty data={prettyData}/>
     </div>
   );
 }
---
+++ src/components/Pretty/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import './index.css';
+
+interface Props {
+    data: string;
+}
+
+function Pretty({data}: Props) {
+    const json = JSON.parse(data);
+    return (
+        <div className="pretty">
+            <Inner data={json}/>
+        </div>
+    );
+}
+
+interface InnerProps {
+    data: any;
+    newLine?:boolean;
+}
+
+function Inner({data,newLine=true}: InnerProps) {
+    if (typeof data === "string") {
+        return newLine?<p>{data}</p>:<span>{data}</span>
+    } else if (Array.isArray(data)) {
+        return (
+            <div>
+                <div>[</div>
+                <div className="item">
+                    {data.map((item: any) => {
+                        return <Inner key={item} data={item}/>
+                    })}
+                </div>
+                <div>]</div>
+            </div>)
+    } else if (typeof data === "object") {
+        return (
+            <div>
+                <div>{"{"}</div>
+                <div className="item">
+                    {Object.keys(data).map((key, index) => {
+                        return (<div key={key}>{key}:<Inner data={data[key]} newLine={false}/></div>)
+                    })}
+                </div>
+                <p>{"}"}</p>
+            </div>)
+    } else {
+        return <div></div>
+    }
+}
+
+export default Pretty;
